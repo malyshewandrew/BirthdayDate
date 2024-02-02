6eba1a485bd8bd7bfe573ad605aec551b1425f6f
malyshewandrew <malyshewandrew@protonmail.com>
2024-02-02 13:19:38 +0300

    create CoreDataManager for project

---
+++ BirthdayDate/CoreDataManager.swift
@@ -0,0 +1,73 @@
+import CoreData
+import UIKit.UIApplication
+
+enum CoreDataError: Error {
+    case error(String)
+}
+
+final class CoreDataManager {
+    static let instance = CoreDataManager()
+    private init() {}
+
+    // MARK: - SAVE USER:
+
+    func saveUser(name: String, surname: String, date: Date) -> Result<Void, CoreDataError> {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return .failure(.error("AppDelegate not found"))
+        }
+
+        let managedContext = appDelegate.persistentContainer.viewContext
+
+        let entity = NSEntityDescription.entity(forEntityName: "User", in: managedContext)!
+
+        let user = NSManagedObject(entity: entity, insertInto: managedContext)
+
+        user.setValue(name, forKey: "name")
+        user.setValue(surname, forKey: "surname")
+        user.setValue(date, forKey: "date")
+
+        do {
+            try managedContext.save()
+            return .success(())
+        } catch {
+            return .failure(.error("Could not save. \(error)"))
+        }
+    }
+    
+    // MARK: - GET USERS:
+
+    func getUsers() -> Result<[User], CoreDataError> {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return .failure(.error("AppDelegate not found"))
+        }
+
+        let managedContext = appDelegate.persistentContainer.viewContext
+
+        let fetchRequest = NSFetchRequest<User>(entityName: "User")
+
+        do {
+            let objects = try managedContext.fetch(fetchRequest)
+            return .success(objects)
+        } catch {
+            return .failure(.error("Could not fetch \(error)"))
+        }
+    }
+    
+    // MARK: DELETE USER:
+
+    func deleteUser(_ user: User) -> Result<Void, CoreDataError> {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return .failure(.error("AppDelegate not found"))
+        }
+
+        let managedContext = appDelegate.persistentContainer.viewContext
+
+        do {
+            managedContext.delete(user)
+            try managedContext.save()
+            return .success(())
+        } catch {
+            return .failure(.error("Error deleting user: \(error)"))
+        }
+    }
+}
